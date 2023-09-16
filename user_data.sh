696a16a162d9dcb21d08242b83d26eaea19aef46
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-16 11:58:56 +0530

    error from server.js file

--- user_data.sh
+++ user_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-labauto ansible
- ansible-pull  -e host=localhost, -U https://github.com/chandra5141/roboshop_ansible.git roboshop.yml  -e role_name=${component} -e env=${env} | tee /opt/ansible.log
+sudo labauto ansible
+sudo ansible-pull  -e host=localhost, -U https://github.com/chandra5141/roboshop_ansible.git roboshop.yml  -e role_name=${component} -e env=${env} | tee /opt/ansible.log
