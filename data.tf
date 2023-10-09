c0db779bb8fdf855e31e283c6d61bbea30075535
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-10-09 12:17:32 +0530

    adding_certification_arn

--- data.tf
+++ data.tf
@@ -9,9 +9,17 @@ data "aws_kms_key" "key" {
   key_id = "alias/roboshop"
 }
 
+#data "aws_ami" "ami_id" {
+#  most_recent      = true
+#  name_regex       = "Centos-8-DevOps-Practice"
+#  owners           = ["973714476881"]
+#
+#}
+
+# created own snapshot of ami to provision instance faster with  ansible software
 data "aws_ami" "ami_id" {
   most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
+  name_regex       = "roboshop_internal"
+  owners           = ["044366763273"]
 
 }
